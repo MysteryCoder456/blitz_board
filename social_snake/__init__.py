069323ed23a8aa92888a0abd6f44c18ef9fe042d
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-24 22:26:22 +0400

    sqlalchemy + alembic for db migrations

--- social_snake/__init__.py
+++ social_snake/__init__.py
@@ -1,6 +1,17 @@
+import os
+from dotenv import load_dotenv
 from quart import Quart, render_template
+from quart_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Quart(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
+
+db = SQLAlchemy(app)
+
+from . import models
 
 
 @app.route("/")
