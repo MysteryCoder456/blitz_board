ca3154930d2975f569f2430c27f2f720c216f77b
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-27 23:01:14 +0400

    fixed broken ass UI (i hate css) & wtforms

--- social_snake/__init__.py
+++ social_snake/__init__.py
@@ -2,6 +2,11 @@ import os
 from dotenv import load_dotenv
 from quart import Quart, render_template
 from quart_sqlalchemy import SQLAlchemy
+from quart_wtf.csrf import (
+    CSRFProtect,
+    DEFAULT_SUBMIT_METHODS,
+    DEFAULT_CSRF_FIELD_NAME,
+)
 
 load_dotenv()
 
@@ -9,6 +14,13 @@ app = Quart(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
 
+# Dumb CSRF config without which I get weird errors
+app.config["WTF_CSRF_ENABLED"] = True
+app.config["WTF_CSRF_CHECK_DEFAULT"] = True
+app.config["WTF_CSRF_METHODS"] = DEFAULT_SUBMIT_METHODS
+app.config["WTF_CSRF_FIELD_NAME"] = DEFAULT_CSRF_FIELD_NAME
+
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 
 from . import models
--- social_snake/auth.py
+++ social_snake/auth.py
@@ -1,8 +1,31 @@
-from quart import Blueprint, render_template
+from quart import Blueprint, render_template, request
+from quart_wtf import QuartForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Email
+
+
+class LoginForm(QuartForm):
+    email = StringField(label="Email", validators=[Email()])
+    submit = SubmitField()
+
 
 auth_bp = Blueprint(
     "auth", __name__, url_prefix="/auth", template_folder="templates/auth"
 )
+
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+async def login():
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(await request.form)
+
+        if await form.validate_on_submit():
+            print(f"Emailing link to {form.email.data}")
+            # TODO: Do the link sending
+
+    return await render_template("login.html", form=form)
 
 
 @auth_bp.route("/signup")
