610ccc61df3845513d175a2a1c0ee54db6159d5c
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-05-20 11:50:21 +0400

    grid

--- social_snake/static/editor.js
+++ social_snake/static/editor.js
@@ -1,14 +1,34 @@
-const BLOCK_SIZE = 15;
+const BLOCK_SIZE = 40;
 
 function setup() {
-    createCanvas(1920, 1080);
+    createCanvas(1280, 720);
     select("canvas").removeAttribute("style");
+    cursor(CROSS);
 }
 
 function draw() {
     background(0);
+    strokeWeight(2.5);
 
-    textSize(52);
-    fill(255);
-    text("Hello world", 960, 540);
+    // Horizontal Gridlines
+    for (let y = 1; y < height / BLOCK_SIZE; y++) {
+        if (y % 2 == 0) {
+            stroke(200);
+        } else {
+            stroke(100);
+        }
+
+        line(0, y * BLOCK_SIZE, width, y * BLOCK_SIZE);
+    }
+
+    // Vertical Gridlines
+    for (let x = 1; x < width / BLOCK_SIZE; x++) {
+        if (x % 2 == 0) {
+            stroke(200);
+        } else {
+            stroke(100);
+        }
+
+        line(x * BLOCK_SIZE, 0, x * BLOCK_SIZE, height);
+    }
 }
