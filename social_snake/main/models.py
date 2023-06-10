1fca7d6e1f00c9d6a18a5246822dd20290a413aa
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-06-11 00:05:45 +0400

    something bro idk

--- social_snake/main/models.py
+++ social_snake/main/models.py
@@ -1,18 +1,4 @@
-from sqlalchemy.sql.functions import now
 from .. import db
 
 
-class Maze(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    author_id = db.Column(db.ForeignKey("users.id", ondelete="CASCADE"))
-    date_created = db.Column(
-        db.DateTime(timezone=True),
-        nullable=False,
-        default=now(),
-    )
-    difficulty = db.Column(db.Integer, nullable=True)
-    maze_data = db.Column(db.JSON, nullable=False, default={"blocks": {}})
-    published = db.Column(db.Boolean, nullable=False, default=False)
-
-    author = db.relationship("User", back_populates="mazes")
+# TODO:
