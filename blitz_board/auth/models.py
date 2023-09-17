1d0f3bd5374f93e84a5b9acd225b35c9648151a3
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-09-17 22:17:42 +0400

    added friend requests table

--- blitz_board/auth/models.py
+++ blitz_board/auth/models.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from flask_login import UserMixin
+from sqlalchemy.sql.functions import current_timestamp
 
 from .. import db, login_manager
 
@@ -13,6 +14,12 @@ class User(db.Model, UserMixin):
     avatar = db.Column(db.String, nullable=True)
 
     sessions = db.relationship("SessionStats", back_populates="user")
+    outgoing_requests = db.relationship(
+        "FriendRequest", back_populates="from_user"
+    )
+    incoming_requests = db.relationship(
+        "FriendRequest", back_populates="to_user"
+    )
 
     def __str__(self):
         return self.username
@@ -36,3 +43,22 @@ class MagicLink(db.Model):
     valid_until = db.Column(db.DateTime, nullable=False)
 
     user = db.relationship(User)
+
+
+class FriendRequest(db.Model):
+    __tablename__ = "friend_requests"
+
+    from_id = db.Column(db.ForeignKey(User.id), primary_key=True)
+    to_id = db.Column(db.ForeignKey(User.id), primary_key=True)
+    timestamp = db.Column(
+        db.DateTime(timezone=True),
+        nullable=False,
+        default=current_timestamp,
+    )
+
+    from_user = db.relationship(
+        User, foreign_keys=[from_id], back_populates="outgoing_requests"
+    )
+    to_user = db.relationship(
+        User, foreign_keys=[to_id], back_populates="incoming_requests"
+    )
