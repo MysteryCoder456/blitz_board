71581cd25009d42266f790645ec56544b9c89679
Rehatbir Singh <rehatbir.singh@gmail.com>
2023-10-12 15:57:00 +0400

    multithreaded docker server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ${VENV_PATH}/bin/poetry run alembic upgrade head
-${VENV_PATH}/bin/poetry run gunicorn -k eventlet "blitz_board:app" -b web:5000
+${VENV_PATH}/bin/poetry run gunicorn -k eventlet "blitz_board:app" -b web:5000 --threads $(nproc)
