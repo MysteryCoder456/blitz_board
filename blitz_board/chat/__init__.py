9dc97bfba59a33560554d3e811b502934ac57ac3
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-10-30 14:08:15 +0400

    added message sending

--- blitz_board/chat/__init__.py
+++ blitz_board/chat/__init__.py
@@ -2,8 +2,9 @@ from pathlib import Path
 from typing import Any
 from flask import Blueprint, render_template, url_for
 from flask_login import login_required, current_user
+from flask_socketio import emit, join_room
 
-from .. import app, db
+from .. import app, db, socketio
 from .models import Channel, Message
 from ..auth.models import User
 from ..auth.signals import friend_added
@@ -16,12 +17,65 @@ chat_bp = Blueprint(
     template_folder=templates,
 )
 
+open_channels: dict[int, Channel] = {}
+
 
 @friend_added.connect_via(app)
 def create_chat_channel(sender: Any, left_id: int, right_id: int, **extra):
     # Create a channel for the two users
     new_channel = Channel(member_one_id=left_id, member_two_id=right_id)  # type: ignore
     db.session.add(new_channel)
+    db.session.commit()
+
+
+@socketio.on("connect", namespace="/chat")
+def on_connect(auth: dict):
+    channel_id: int = auth["channel_id"]
+
+    query = db.select(Channel).where(
+        (Channel.id == channel_id)
+        & (
+            (Channel.member_one == current_user)
+            | (Channel.member_two == current_user)
+        )
+    )
+    channel = db.session.execute(query).scalar()
+
+    if not channel:
+        return False
+
+    open_channels[current_user.id] = channel  # type: ignore
+    join_room(channel.id)
+
+
+@socketio.on("disconnect", namespace="/chat")
+def on_disconnect():
+    del open_channels[current_user.id]  # type: ignore
+
+
+@socketio.on("send message", namespace="/chat")
+def on_send(msg_content: str):
+    channel = open_channels[current_user.id]  # type: ignore
+    new_msg = Message(  # type: ignore
+        author=current_user,
+        channel=channel,
+        content=msg_content.strip(),
+    )
+
+    author_avatar = (
+        url_for("site_media", media_path=new_msg.author.avatar)
+        if new_msg.author.avatar
+        else None
+    )
+    new_msg_json = {
+        "author": new_msg.author.username,
+        "author_avatar": author_avatar,
+        "content": new_msg.content,
+        "timestamp": new_msg.timestamp,
+    }
+
+    emit("new message", new_msg_json, to=channel.id)
+    db.session.add(new_msg)
     db.session.commit()
 
 
@@ -67,5 +121,6 @@ def chat_page(user_id: int):
         user=user,
         channel=channel,
         messages=messages,
+        socket_io=url_for("static", filename="js/socket.io.js"),
         chat_script=url_for("static", filename="js/chat.js"),
     )
--- blitz_board/static/js/chat.js
+++ blitz_board/static/js/chat.js
@@ -1,3 +1,29 @@
 const msgBox = document.querySelector("#msg-box");
+const sendInput = document.querySelector("#send-input");
+const sendBtn = document.querySelector("#send-btn");
+
+const socket = io("/chat", {
+    auth: {
+        channel_id: channelId,
+    },
+});
+
+function sendMessage() {
+    const msgContent = sendInput.value.trim();
+
+    if (msgContent.length === 0) {
+        return;
+    }
+
+    socket.emit("send message", msgContent);
+    sendInput.value = "";
+}
 
 msgBox.scrollTop = msgBox.scrollHeight + 50;
+sendBtn.onclick = () => sendMessage();
+
+document.onkeydown = (ev) => {
+    if (ev.key === "Enter") {
+        sendMessage();
+    }
+}
