e493ba8c8c4b535d16e6c05e60bfc8cc127310c4
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-11-08 18:57:13 +0400

    added friends leaderboard tab

--- blitz_board/leaderboard/__init__.py
+++ blitz_board/leaderboard/__init__.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from flask import Blueprint, render_template
+from flask_login import current_user
 from sqlalchemy.sql import func
 
 from .. import db
-from ..auth.models import User
+from ..auth.models import User, Friends
 from ..game.models import SessionStats
 
 templates = Path(__file__).parent / "templates"
@@ -16,7 +17,7 @@ leaderboard_bp = Blueprint(
 
 
 @leaderboard_bp.route("/global")
-def global_leaderboard():
+def global_lb():
     query = (
         db.select(User, func.max(SessionStats.speed))
         .where(User.id == SessionStats.user_id)
@@ -27,6 +28,34 @@ def global_leaderboard():
 
     return render_template(
         "leaderboard.html",
+        lb_name="Global",
+        ranked_players=ranked_players,
+        enumerate=enumerate,
+        round=round,
+    )
+
+
+@leaderboard_bp.route("/friends")
+def friends_lb():
+    query = (
+        db.select(User, func.max(SessionStats.speed))
+        .where(User.id == SessionStats.user_id)
+        .group_by(User.id)
+        .order_by(func.max(SessionStats.speed).desc())
+    )
+    ranked_players = db.session.execute(query).all()
+
+    # HACK: try to integrate this logic into SQL query
+    ranked_players = [
+        player
+        for player in ranked_players
+        if Friends.check_friends(player[0].id, current_user.id)  # type: ignore
+        or player[0] == current_user
+    ]
+
+    return render_template(
+        "leaderboard.html",
+        lb_name="Friends",
         ranked_players=ranked_players,
         enumerate=enumerate,
         round=round,
