9c05018ed39ed3f6966a18b98966bed4fcab614b
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-11-08 19:18:20 +0400

    much better friends leaderboard implementation

--- blitz_board/leaderboard/__init__.py
+++ blitz_board/leaderboard/__init__.py
@@ -18,6 +18,7 @@ leaderboard_bp = Blueprint(
 
 @leaderboard_bp.route("/global")
 def global_lb():
+    # Rank users based on speed
     query = (
         db.select(User, func.max(SessionStats.speed))
         .where(User.id == SessionStats.user_id)
@@ -37,21 +38,28 @@ def global_lb():
 
 @leaderboard_bp.route("/friends")
 def friends_lb():
-    query = (
+    # Retreive friend IDs
+    friends_query = db.select(Friends).where(
+        (Friends.left_user == current_user)
+        | (Friends.right_user == current_user)
+    )
+    friends = db.session.execute(friends_query).scalars()
+    friend_ids = [
+        f.left_id if f.right_user == current_user else f.right_id
+        for f in friends
+    ]
+
+    # Rank friends based on speed
+    players_query = (
         db.select(User, func.max(SessionStats.speed))
-        .where(User.id == SessionStats.user_id)
+        .where(
+            (User.id == SessionStats.user_id)
+            & ((User.id == current_user.id) | User.id.in_(friend_ids))  # type: ignore
+        )
         .group_by(User.id)
         .order_by(func.max(SessionStats.speed).desc())
     )
-    ranked_players = db.session.execute(query).all()
-
-    # HACK: try to integrate this logic into SQL query
-    ranked_players = [
-        player
-        for player in ranked_players
-        if Friends.check_friends(player[0].id, current_user.id)  # type: ignore
-        or player[0] == current_user
-    ]
+    ranked_players = db.session.execute(players_query).all()
 
     return render_template(
         "leaderboard.html",
