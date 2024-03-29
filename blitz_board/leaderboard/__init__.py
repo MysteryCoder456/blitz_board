15e7e44ed003ec26a5d57405f7d7d596b6190180
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-03-29 09:16:17 +0400

    added average speed option in leaderboard

--- blitz_board/leaderboard/__init__.py
+++ blitz_board/leaderboard/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 from sqlalchemy.sql import func
 
@@ -18,18 +18,30 @@ leaderboard_bp = Blueprint(
 
 @leaderboard_bp.route("/global")
 def global_lb():
-    # Rank users based on speed
-    query = (
-        db.select(User, func.max(SessionStats.speed))
-        .where(User.id == SessionStats.user_id)
-        .group_by(User.id)
-        .order_by(func.max(SessionStats.speed).desc())
-    )
+    rank_by_avg = "avg" in request.args
+
+    if rank_by_avg:
+        # Rank users based on average speed
+        query = (
+            db.select(User, func.avg(SessionStats.speed))
+            .where(User.id == SessionStats.user_id)
+            .group_by(User.id)
+            .order_by(func.avg(SessionStats.speed).desc())
+        )
+    else:
+        # Rank users based on maximum speed
+        query = (
+            db.select(User, func.max(SessionStats.speed))
+            .where(User.id == SessionStats.user_id)
+            .group_by(User.id)
+            .order_by(func.max(SessionStats.speed).desc())
+        )
     ranked_players = db.session.execute(query).all()
 
     return render_template(
         "leaderboard.html",
         lb_name="Global",
+        rank_by_avg=rank_by_avg,
         ranked_players=ranked_players,
         enumerate=enumerate,
         round=round,
@@ -39,6 +51,8 @@ def global_lb():
 @leaderboard_bp.route("/friends")
 @login_required
 def friends_lb():
+    rank_by_avg = "avg" in request.args
+
     # Retreive friend IDs
     friends_query = db.select(Friends).where(
         (Friends.left_user == current_user)
@@ -50,21 +64,34 @@ def friends_lb():
         for f in friends
     ]
 
-    # Rank friends based on speed
-    players_query = (
-        db.select(User, func.max(SessionStats.speed))
-        .where(
-            (User.id == SessionStats.user_id)
-            & ((User.id == current_user.id) | User.id.in_(friend_ids))  # type: ignore
+    if rank_by_avg:
+        # Rank friends based on average speed
+        players_query = (
+            db.select(User, func.avg(SessionStats.speed))
+            .where(
+                (User.id == SessionStats.user_id)
+                & ((User.id == current_user.id) | User.id.in_(friend_ids))  # type: ignore
+            )
+            .group_by(User.id)
+            .order_by(func.avg(SessionStats.speed).desc())
         )
-        .group_by(User.id)
-        .order_by(func.max(SessionStats.speed).desc())
-    )
+    else:
+        # Rank friends based on maximum speed
+        players_query = (
+            db.select(User, func.max(SessionStats.speed))
+            .where(
+                (User.id == SessionStats.user_id)
+                & ((User.id == current_user.id) | User.id.in_(friend_ids))  # type: ignore
+            )
+            .group_by(User.id)
+            .order_by(func.max(SessionStats.speed).desc())
+        )
     ranked_players = db.session.execute(players_query).all()
 
     return render_template(
         "leaderboard.html",
         lb_name="Friends",
+        rank_by_avg=rank_by_avg,
         ranked_players=ranked_players,
         enumerate=enumerate,
         round=round,
